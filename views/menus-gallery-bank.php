4c57840bccc92a2f0ea0e8e4338ccaa28fcbb499
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 07:49:52 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/tags/1.3@720268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/menus-gallery-bank.php
@@ -0,0 +1,14 @@
+<div id="top">
+	<h1 id="logo"></h1>
+	<a href="http://gallery-bank.com/" target="_blank"><img style="margin-left:40px;margin-bottom:10px;" src="<?php echo plugins_url('../gfx/notice.png',__FILE__) ?>"/></a>
+	<div id="menu">
+		<ul class="sf-js-enabled">
+			<li id="gallery_bank">
+				<a href="admin.php?page=gallery_bank">
+					<?php _e( "Gallery Bank", gallery_bank ); ?>
+				</a>
+			</li> 
+			
+		</ul>
+	</div>
+</div>
