80ed72e4ec9bd88d44b1909750eaed43d38de576
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 04:23:50 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/trunk@742589 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/settings.php
+++ views/settings.php
@@ -1,3 +1,3 @@
 <a href="http://gallery-bank.com/" target="_blank">
-	<img src="<?php echo GALLERY_BK_PLUGIN_URL .'/assets/images/settings.png';?>" style="width:1106px;" />
+	<img src="<?php echo GALLERY_BK_PLUGIN_URL .'/assets/images/settings.png';?>"  />
 </a>
