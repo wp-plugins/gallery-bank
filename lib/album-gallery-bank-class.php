0a747a2a57646315ef8a630dbafbe063992f7635
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-24 10:38:39 +0000

    New version  release
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/tags/2.0.3@745605 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/album-gallery-bank-class.php
@@ -0,0 +1,296 @@
+<?php
+global $wpdb;
+global $current_user;
+$current_user = wp_get_current_user();
+if (!current_user_can("edit_posts") && ! current_user_can("edit_pages"))
+{
+	return;
+}
+else
+{
+	if(isset($_REQUEST["param"]))
+	{
+		if($_REQUEST["param"] == "add_new_album")
+		{
+			$ux_album_name = html_entity_decode(esc_attr($_REQUEST["album_name"]));
+			$ux_desciption = html_entity_decode(esc_attr($_REQUEST["ux_description"]));
+			$cover = esc_attr($_REQUEST["cover_array"]);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"INSERT INTO ".gallery_bank_albums()."(album_name, description, album_date, author)
+					VALUES(%s, %s, CURDATE(), %s)",
+					$ux_album_name,
+					$ux_desciption,
+					$current_user->display_name
+				)
+			);
+			echo $EventLastId=$wpdb->insert_id;
+			$setting = "image_size:1;width:160px;height:120px;images_in_row:3;image_opacity:1;image_border_size:2px;image_border_radius:2px;image_border_color:rgb(0, 0, 0);/cover_size:1;width:160px;height:120px;cover_opacity:1;cover_border_size:2px;cover_border_radius:2px;border_color:rgb(0, 0, 0);/overlay_opacity:0.6;overlay_border_size:0px;overlay_border_radius:0px;text_color:rgb(0, 0, 0);overlay_border_color:rgb(255, 255, 255);inline_bg_color:rgb(255, 255, 255);overlay_bg_color:rgb(0, 0, 0);/autoplay:0;slide_interval:2;/pagination:0;";
+			if($cover == "undefined")
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"INSERT INTO ".gallery_bank_settings()."(album_id, album_settings, setting_content)
+						VALUES(%d, %d, %s)",
+						$EventLastId,
+						1,
+						$setting
+					)
+				);
+			}
+			else
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"INSERT INTO ".gallery_bank_settings()."(album_id, album_settings, setting_content, album_cover)
+						VALUES(%d, %d, %s, %s)",
+						$EventLastId,
+						1,
+						$setting,
+						$cover
+					)
+				);
+			}
+			die();
+		}
+		elseif($_REQUEST["param"] == "update_album")
+		{
+			$albumId = intval($_REQUEST["albumId"]);
+			$ux_edit_album_name = html_entity_decode(esc_attr($_REQUEST["edit_album_name"]));
+			$ux_edit_desciption = html_entity_decode(esc_attr($_REQUEST["ux_edit_description"]));
+			$edit_cover = esc_attr($_REQUEST["cover_array"]);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"UPDATE " .gallery_bank_albums(). " SET album_name = %s, description = %s WHERE album_id = %d",
+					$ux_edit_album_name,
+					$ux_edit_desciption,
+					$albumId
+				)
+			);
+			$setting = "image_size:1;width:160px;height:120px;images_in_row:3;image_opacity:1;image_border_size:2px;image_border_radius:2px;image_border_color:rgb(0, 0, 0);/cover_size:1;width:160px;height:120px;cover_opacity:1;cover_border_size:2px;cover_border_radius:2px;border_color:rgb(0, 0, 0);/overlay_opacity:0.6;overlay_border_size:0px;overlay_border_radius:0px;text_color:rgb(0, 0, 0);overlay_border_color:rgb(255, 255, 255);inline_bg_color:rgb(255, 255, 255);overlay_bg_color:rgb(0, 0, 0);/autoplay:0;slide_interval:2;/pagination:0;";
+			if($edit_cover == "undefined")
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"UPDATE ".gallery_bank_settings()." SET album_settings = %d ,setting_content = %s WHERE album_id = %d",
+						1,
+						$setting,
+						$albumId
+					)
+				);
+			}
+			else
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"UPDATE ".gallery_bank_settings()." SET album_cover = %s, album_settings = %d ,setting_content = %s WHERE album_id = %d",
+						$edit_cover,
+						1,
+						$setting,
+						$albumId
+					)
+				);
+			}
+			die();
+		}
+		else if($_REQUEST['param'] == "Delete_album")
+		{
+			$album_id = intval($_REQUEST['album_id']);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"DELETE FROM ".gallery_bank_pics()." WHERE album_id = %d",
+					$album_id
+				)
+			);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"DELETE FROM ".gallery_bank_albums()." WHERE album_id = %d",
+					$album_id
+				)
+			);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"DELETE FROM ".gallery_bank_settings()." WHERE album_id = %d",
+					$album_id
+				)
+			);
+			die();
+		}
+		else if($_REQUEST["param"] == "add_pic")
+		{
+			$ux_path = esc_attr($_REQUEST["path"]);
+			$ux_albumid = intval($_REQUEST["album_id"]);
+			$ux_title = html_entity_decode($_REQUEST["title"]);
+			$ux_detail = html_entity_decode($_REQUEST["detail"]);
+			$thumbnail_url = esc_attr($_REQUEST["thumb"]);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"INSERT INTO ".gallery_bank_pics()."(album_id,pic_path,thumbnail_url,title,description,date)
+					VALUES(%d,%s,%s,%s,%s,CURDATE())",
+					$ux_albumid,
+					$ux_path,
+					$thumbnail_url,
+					$ux_title,
+					$ux_detail
+				)
+			);
+			$pic_id = $wpdb->insert_id;
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"UPDATE " .gallery_bank_pics(). " SET sorting_order = %d WHERE pic_id = %d",
+					$pic_id,
+					$pic_id
+				)
+			);
+			die();
+		}
+		else if($_REQUEST["param"] == "update_pic")
+		{
+			$pic_id = intval($_REQUEST['picId']);
+			$albumId = intval($_REQUEST['albumId']);
+			$edit_title = html_entity_decode($_REQUEST['edit_title']);
+			$edit_detail = html_entity_decode($_REQUEST['edit_detail']);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"UPDATE " .gallery_bank_pics(). " SET title = %s, description= %s WHERE pic_id = %d",
+					$edit_title,
+					$edit_detail,
+					$pic_id
+				)
+			);
+			
+			die();
+		}
+		else if($_REQUEST['param'] == "delete_pic")
+		{
+			$picture_id = $_REQUEST['id'];
+			$count_pic = intval($_REQUEST['count_pic']);
+			$pic_id =  (explode(",",$picture_id));
+			for($flag = 0; $flag < $count_pic; $flag++ )
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"DELETE FROM ".gallery_bank_pics()." WHERE pic_id = %d",
+						$pic_id[$flag]
+					)
+				);
+			}
+			die();
+		}
+		else if($_REQUEST['param'] == "delete_album")
+		{
+			$album_id = intval($_REQUEST['album_id']);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"DELETE FROM ".gallery_bank_pics()." WHERE album_id = %d",
+					$album_id
+				)
+			);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"DELETE FROM ".gallery_bank_settings()." WHERE album_id = %d",
+					$album_id
+				)
+			);
+			$wpdb->query
+			(
+				$wpdb->prepare
+				(
+					"DELETE FROM ".gallery_bank_albums()." WHERE album_id = %d",
+					$album_id
+				)
+			);
+			die();
+		}
+		
+		else if($_REQUEST["param"] == "reorderControls")
+		{
+			
+			$updateRecordsArray = $_POST['recordsArray'];
+			$listingCounter = 1;
+			foreach ($updateRecordsArray as $recordIDValue)
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"UPDATE ".gallery_bank_pics()." SET sorting_order = %d WHERE pic_id = %d",
+						$listingCounter,
+						$recordIDValue
+					)
+				);
+				$listingCounter = $listingCounter + 1;	
+			}
+			die();
+		}
+		else if($_REQUEST['param'] == "delete_all_albums")
+		{
+			$album = $wpdb->get_results
+			(
+				$wpdb->prepare
+				(
+					"SELECT * FROM ".gallery_bank_albums(),""
+				)
+			);
+			for($flag=0; $flag < count($album); $flag++)
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"DELETE FROM ".gallery_bank_pics()." WHERE album_id = %d",
+						$album[$flag]->album_id
+					)
+				);
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"DELETE FROM ".gallery_bank_settings()." WHERE album_id = %d",
+						$album[$flag]->album_id
+					)
+				);
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"DELETE FROM ".gallery_bank_albums()." WHERE album_id = %d",
+						$album[$flag]->album_id
+					)
+				);
+			}
+			die();
+		}
+	}
+}
+?>
---
+++ views/front-view-albums.php
@@ -0,0 +1,154 @@
+<?php
+global $wpdb;
+	$album = $wpdb->get_row
+	(
+		$wpdb->prepare
+		(
+		"SELECT * FROM ".gallery_bank_albums()." where album_id = %d",
+		$album_id
+		)
+	);
+	$unique_id = rand(100,10000);
+	$get_settings = $wpdb->get_var
+	(
+		$wpdb->prepare
+		(
+		"SELECT album_settings FROM ". gallery_bank_settings(). " WHERE album_id = %d ",
+		$album_id
+		)
+	);
+	if($get_settings == 1)
+	{
+		$sett_val = $wpdb->get_row
+		(
+			$wpdb->prepare
+			(
+			"SELECT * FROM ". gallery_bank_settings(). " WHERE album_settings = %d and album_id = %d",
+			$get_settings,
+			0
+			)
+		);
+	}
+	else
+	{
+		$sett_val = $wpdb->get_row
+		(
+			$wpdb->prepare
+			(
+			"SELECT * FROM ". gallery_bank_settings(). " WHERE album_settings = %d and album_id =%d",
+			$get_settings,
+			$album_id
+			)
+		);
+	}
+	$setting_cover = $wpdb->get_row
+	(
+		$wpdb->prepare
+		(
+			"SELECT * FROM ". gallery_bank_settings(). " WHERE album_settings = %d and album_id = %d",
+			$get_settings,
+			$album_id
+		)
+	);
+	$content = explode("/", $sett_val->setting_content);
+	$cover_settings = explode(";", $content[1]);
+	$cover_content = explode(":", $cover_settings[0]);
+	$cover_width = explode(":", $cover_settings[1]);
+	$cover_height = explode(":", $cover_settings[2]);
+	$cover_opacity = explode(":", $cover_settings[3]);
+	$cover_border_size = explode(":", $cover_settings[4]);
+	$cover_border_radius = explode(":", $cover_settings[5]);
+	$cover_border_color = explode(":", $cover_settings[6]);
+	$cover_border_value = $cover_border_size[1] ." solid " . $cover_border_color[1];
+	$pagination_settings = explode(";", $content[4]);
+	$pagination = explode(":", $pagination_settings[0]);
+?>
+<button id="back_button<?php echo $unique_id;?>" style="margin-top:10px; display: none;" onclick="view_albums<?php echo $unique_id;?>();">
+	<span style="color: #000;"> &laquo <?php _e('Back to Albums', gallery_bank); ?></span>
+</button>
+<?php
+if(($setting_cover->album_cover == "undefined") || ($setting_cover->album_cover == "") )
+{
+	$url = GALLERY_BK_PLUGIN_URL."/album-cover.png";
+	?>
+	<div id="main_div<?php echo $unique_id;?>" style="display: block;" class="album-cover">
+		<?php
+			$album_custom_cover_css = "border:"  . $cover_border_value . ";border-radius:" . $cover_border_radius[1].";opacity:". $cover_opacity[1];
+		?>
+		<img class="imgHolder" src="<?php echo stripcslashes($url); ?>" onclick="view_images_<?php echo $unique_id;?>(<?php echo $album_id;?>);" style="cursor:pointer;<?php echo $album_custom_cover_css; ?>" />	
+		<div style="text-align: justify;display:inline-block;vertical-align:top;margin-left:20px;">
+			<h3><?php echo stripcslashes($album->album_name); ?>&nbsp;</h3>
+			<span><?php echo stripcslashes($album->description);?>&nbsp;</span><br/>
+			<a style="cursor: pointer;" onclick="view_images_<?php echo $unique_id;?>(<?php echo $album_id;?>)">
+				<?php _e("See Album images", gallery_bank); ?> &raquo
+			</a>
+		</div>
+	</div>
+	<?php
+}
+else 
+{
+	$domain = strstr($setting_cover->album_cover, '/wp-content');
+	?>
+	<div id="main_div<?php echo $unique_id;?>" style="display: block;" class="album-cover">
+		<?php
+		if($cover_content[1] == 1)
+		{
+			
+				$album_custom_cover_css = "border:" . $cover_border_value . ";border-radius:" . $cover_border_radius[1].";opacity:". $cover_opacity[1];
+				?>
+				<img class="imgHolder" src="<?php echo stripcslashes(GALLERY_BK_PLUGIN_URL).'/lib/timthumb.php?src='.stripcslashes($setting_cover->album_cover).'&h=150&w=150&zc=1&q=100';?>" onclick="view_images_<?php echo $unique_id;?>(<?php echo $album_id;?>);" style="cursor:pointer;<?php echo $album_custom_cover_css; ?>" />
+				<?php
+			
+		}
+		else 
+		{
+			
+				$album_cover_css = "border:" . $cover_border_value . ";border-radius:" . $cover_border_radius[1].";opacity:". $cover_opacity[1];
+				?>
+				<img class="imgHolder" src="<?php echo stripcslashes(GALLERY_BK_PLUGIN_URL).'/lib/timthumb.php?src='.stripcslashes($setting_cover->album_cover).'&h='.$cover_height[1].'&w='.$cover_width[1].'&zc=1&q=100';?>" onclick="view_images_<?php echo $unique_id;?>(<?php echo $album_id;?>);" style="cursor:pointer;<?php echo $album_cover_css; ?>" />
+				<?php
+			
+		}
+		?>
+		<div style="text-align: justify;display:inline-block;vertical-align:top;margin-left:20px;">
+			<h3><?php echo stripcslashes($album->album_name); ?>&nbsp;</h3>
+			<span><?php echo stripcslashes($album->description);?>&nbsp;</span><br/>
+			<a style="cursor: pointer;" onclick="view_images_<?php echo $unique_id;?>(<?php echo $album_id;?>)">
+				<?php _e("See Album images", gallery_bank ); ?> &raquo
+			</a>
+		</div>
+	</div>
+	<?php
+}
+?>
+<div id="image_show_div<?php echo $unique_id;?>" style="display: none;" class="images-cover">
+	<h3 id="album_title<?php echo $unique_id;?>"><?php echo stripcslashes($album->album_name); ?>&nbsp;</h3>
+	<div id="show_images_<?php echo $unique_id;?>" >
+	</div>
+</div>
+<script type="text/javascript">
+	var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+	function view_images_<?php echo $unique_id;?>(album_id)
+	{
+		jQuery(".album-cover").css('display','none');
+		jQuery("#main_div<?php echo $unique_id;?>").css('display','none');
+		jQuery("#back_button<?php echo $unique_id;?>").css('display','none');
+		jQuery("#image_show_div<?php echo $unique_id;?>").css('display','block');
+		jQuery.post(ajaxurl, "album_id="+album_id+"&param=show_images&action=front_albums_gallery_library", function(data)
+		{
+			jQuery("#back_button<?php echo $unique_id;?>").css('display','block');
+			jQuery('#show_images_<?php echo $unique_id;?>').html(data);
+		});
+		jQuery.post(ajaxurl, "album_id="+album_id+"&param=get_album_name&action=front_albums_gallery_library", function(data)
+		{
+			jQuery("#album_title<?php echo $unique_id;?>").html(data);
+		});
+	}
+	function view_albums<?php echo $unique_id;?>()
+	{
+		jQuery(".album-cover").css('display','block');
+		jQuery(".images-cover").css('display','none');
+		jQuery("#back_button<?php echo $unique_id;?>").css('display','none');
+	}
+</script>
