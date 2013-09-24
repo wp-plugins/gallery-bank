fd58bc0a67e8d2f881da6a8c428ed3f47516d406
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-24 08:17:02 +0000

    new version release
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/trunk@777622 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gallery-bank.php
+++ gallery-bank.php
@@ -4,7 +4,7 @@
  Plugin URI: http://gallery-bank.com
  Description: Gallery Bank is an interactive WordPress photo gallery plugin, best fit for creative and corporate portfolio websites.
  Author: Gallery-Bank
- Version: 2.0.16
+ Version: 2.0.17
  Author URI: http://gallery-bank.com
 */
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
