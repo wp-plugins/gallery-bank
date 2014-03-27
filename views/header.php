1b82786e09c1784a4359763d4c1e73d7007d1673
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-27 04:47:06 +0000

    bug fixed with nl language
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/trunk@882758 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/header.php
+++ views/header.php
@@ -2,9 +2,9 @@
 $gb_lang = array();
 $gb_translated_lang = array();
 array_push($gb_lang, "ar", "bg_BG", "da_DK", "de_DE", "fi_FI", "fr_FR", "he_IL", "hu_HU", "id_ID",
- "it_IT", "ja", "ko_KR", "ms_MY", "nl_NL", "pl_PL", "pt_BR", "pt_PT", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sq_AL",
+ "it_IT", "ja", "ko_KR", "ms_MY", "pl_PL", "pt_BR", "pt_PT", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sq_AL",
  "sr_RS", "sv_SE", "th", "tr", "zh_CN");
-array_push($gb_translated_lang, "en_GB", "en_US", "es_ES");
+array_push($gb_translated_lang, "en_GB", "en_US", "es_ES", "nl_NL");
 $language = get_locale();
 ?>
 
