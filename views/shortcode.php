753c4935e6429ff8f71d189623ed28e91ed3b2b5
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-28 04:32:03 +0000

    New Version Release and Tagging Old Version New Language Added Bugs Fixed
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/tags/3.0.9@883366 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/shortcode.php
@@ -0,0 +1,36 @@
+<form id="shortcode" class="layout-form" method="post">
+    <div class="fluid-layout">
+        <div class="layout-span12">
+            <ul class="breadcrumb">
+                <li>
+                    <i class="icon-home"></i>
+                    <a href="admin.php?page=gallery_bank"><?php _e("Gallery Bank", gallery_bank); ?></a>
+                    <span class="divider">/</span>
+                    <a href="#"><?php _e("Gallery Bank Short-Codes", gallery_bank); ?></a>
+                </li>
+            </ul>
+            <div class="widget-layout">
+                <div class="widget-layout-title">
+                    <h4>
+                        <i class="icon-plus"></i>
+                        <?php _e("How to setup Short-Codes for Gallery Bank into your WordPress Page/Post?", gallery_bank); ?>
+                    </h4>
+                </div>
+                <div class="widget-layout-body">
+                    <a class="btn btn-inverse"
+                       href="admin.php?page=gallery_bank"><?php _e("Back to Albums", gallery_bank); ?></a>
+                    <div class="separator-doubled"></div>
+                    <div class="fluid-layout">
+                        <div class="layout-span12">
+                            <div class="widget-layout">
+                                <div class="widget-layout-body">
+                                	<img src="<?php echo GALLERY_BK_PLUGIN_URL . "/assets/images/how-to-setup-short-code.png";?>" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
