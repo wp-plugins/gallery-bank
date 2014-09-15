c794f5b6b49423923b4398d7ce1cd28ccc2715a0
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 04:28:26 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/trunk@989854 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gallery-bank.php
+++ gallery-bank.php
@@ -4,7 +4,7 @@
  Plugin URI: http://tech-banker.com
  Description: Gallery Bank is an easy to use Responsive WordPress Gallery Plugin for photos, videos, galleries and albums.
  Author: Tech Banker
- Version: 3.0.64
+ Version: 3.0.65
  Author URI: http://tech-banker.com
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
