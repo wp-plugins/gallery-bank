98f0583c07f5c9c89ec56fa728083a8486e35398
Gallery-Bank <Gallery-Bank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-30 07:35:46 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/gallery-bank/trunk@1078789 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/purchase_pro_version.php
+++ views/purchase_pro_version.php
@@ -663,7 +663,7 @@ else
 																class="css3_grid_tooltip"><span>You just need to pay for
 																		once for life time.</span>
 																<h1 class="col1">
-																		&euro;<span>15</span>
+																		&euro;<span>18</span>
 																	</h1>
 																	<h3 class="col1">one time</h3></span></span></span></li>
 													<li
@@ -953,7 +953,7 @@ else
 																class="css3_grid_tooltip"><span>You just need to pay for
 																		once for life time.</span>
 																<h1 class="col1">
-																		&euro;<span>25</span>
+																		&euro;<span>28</span>
 																	</h1>
 																	<h3 class="col1">one time</h3></span></span></span></li>
 													<li
